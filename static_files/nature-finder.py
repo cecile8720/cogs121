9e77e3d2574b90429690870f3f8ece8d53cebdc1
Youngmin Kim <ymjskk96@gmail.com>
2019-05-13 09:23:37 -0700

    stuck on referencing the python file

--- static_files/nature-finder.py
+++ static_files/nature-finder.py
@@ -3,18 +3,21 @@ client_secret = '<d7AMBj94Jj3VZSWNpuIgd6vsr4lDqqSwwoTSLPYVNrxmuVWN>'
 params = {'url': 'http://image.everypixel.com/2014.12/67439828186edc79b9be81a4dedea8b03c09a12825b_b.jpg', 'num_keywords': 10}
 import requests
 keywords = requests.get('https://api.everypixel.com/v1/keywords', params=params, auth=(client_id, client_secret)).json()
+#quality = requests.get('https://api.everypixel.com/v1/quality', params=params, auth=(client_id, client_secret)).json()
+quality = requests.get('https://api.everypixel.com/v1/quality_ugc', params=params, auth=(client_id, client_secret)).json()
 
 
 
 # /keyword function- use GET and POST
+
 with open('image.jpg','rb') as image:
     data = {'data': image}
     keywords = requests.post('https://api.everypixel.com/v1/keywords', files=data, auth=(client_id, client_secret)).json()
 
 # /quality
-with open('image.jpg','rb') as image:
-    data = {'data': image}
-    quality = requests.post('https://api.everypixel.com/v1/quality', files=data, auth=(client_id, client_secret)).json()
+#with open('image.jpg','rb') as image:
+    #data = {'data': image}
+    #quality = requests.post('https://api.everypixel.com/v1/quality', files=data, auth=(client_id, client_secret)).json()
     
 # /quality_ugc
 with open('image.jpg','rb') as image:
