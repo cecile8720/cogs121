9a19a5531a81017819b84a339cf1dadfd2300593
Youngmin Kim <ymjskk96@gmail.com>
2019-05-29 17:34:36 -0700

    nature finder backend working

--- static_files/nature-finder.js
+++ static_files/nature-finder.js
@@ -1,22 +1,23 @@
 const express = require('express')
-var multer = require('multer')
-var upload = multer({dest:'gs://collegeburnouts.appspot.com/'})
+//var multer = require('multer')
+//var upload = multer({dest:'gs://collegeburnouts.appspot.com/'})
 const app = express() 
+const request = require('request')
 
+request("https://OWW16mQtRuHeIKf6X0SSDatK:d7AMBj94Jj3VZSWNpuIgd6vsr4lDqqSwwoTSLPYVNrxmuVWN@api.everypixel.com/v1/keywords?url=http://image.everypixel.com/2014.12/67439828186edc79b9be81a4dedea8b03c09a12825b_b.jpg&num_keywords=10",
+function (error, response, body) {
+    console.log(body);
+    const result = JSON.parse(body);
+    console.log(result);
+});
 
-const param = {'url': 'http://image.everypixel.com/2014.12/67439828186edc79b9be81a4dedea8b03c09a12825b_b.jpg', 'num_keywords': 10}
+const param = {'url': 'https://firebasestorage.googleapis.com/v0/b/collegeburnouts.appspot.com/o/26863598b8bec4c49bea8cf6f805002d.jpg?alt=media&token=2f8fa8c5-78c5-48ee-a4d1-84d9f9eae4a0', 'num_keywords': 10}
 
-/*To get keywords*/
-request.get('https://api.everypixel.com/v1/keywords', 'param'.auth('<OWW16mQtRuHeIKf6X0SSDatK>', '<d7AMBj94Jj3VZSWNpuIgd6vsr4lDqqSwwoTSLPYVNrxmuVWN>'));
-
-const keyword = requests.post('https://api.everypixel.com/v1/keywords');
-/*with open('image.jpg','rb') as image:
-    data = {'data': image}
-    keywords = requests.post('https://api.everypixel.com/v1/keywords', files=data, auth=(client_id, client_secret)).json()*/
-
-
-
-//start the server at URL: http://localhost:8080/
 app.listen(8080, () => {
     console.log('Server started at http://localhost:8080/');
-});
+ });
+ 
+
+//curl --user "OWW16mQtRuHeIKf6X0SSDatK:d7AMBj94Jj3VZSWNpuIgd6vsr4lDqqSwwoTSLPYVNrxmuVWN" "https://api.everypixel.com/v1/keywords?url=http://image.everypixel.com/2014.12/67439828186edc79b9be81a4dedea8b03c09a12825b_b.jpg&num_keywords=10"
+
+//curl  "https://OWW16mQtRuHeIKf6X0SSDatK:d7AMBj94Jj3VZSWNpuIgd6vsr4lDqqSwwoTSLPYVNrxmuVWN@api.everypixel.com/v1/keywords?url=http://image.everypixel.com/2014.12/67439828186edc79b9be81a4dedea8b03c09a12825b_b.jpg&num_keywords=10"
