98c4e421a796ca5d57408d4db571e9b46c7c6c84
Youngmin Kim <ymjskk96@gmail.com>
2019-05-31 16:19:24 -0700

    added multer

--- static_files/nature-finder.js
+++ static_files/nature-finder.js
@@ -1,9 +1,13 @@
 const express = require('express')
-//var multer = require('multer')
-//var upload = multer({dest:'gs://collegeburnouts.appspot.com/'})
+const multer = require('multer')
+const upload = multer({dest:'gs://collegeburnouts.appspot.com/'})
 const app = express() 
 const request = require('request')
 
+const imgURL = //need to pull the url from firebase and or from what the html pulls in
+
+app.post('')
+//the url needs to be able to change, https://... + url + @api...
 request("https://OWW16mQtRuHeIKf6X0SSDatK:d7AMBj94Jj3VZSWNpuIgd6vsr4lDqqSwwoTSLPYVNrxmuVWN@api.everypixel.com/v1/keywords?url=http://image.everypixel.com/2014.12/67439828186edc79b9be81a4dedea8b03c09a12825b_b.jpg&num_keywords=10",
 function (error, response, body) {
     console.log(body);
